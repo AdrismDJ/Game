12db1ee27e22cb1696dff42e5b77f289aa9cf24b
AdrismDJ <53847500+AdrismDJ@users.noreply.github.com>
2022-01-31 08:58:50 -0600

    Hello World Update

--- Assets/HelloWorld.cs
+++ Assets/HelloWorld.cs
@@ -7,12 +7,12 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello"); 
+        Debug.Log("Hello World"); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("World"); 
+        Debug.Log("Hola Mundo"); 
     }
 }
